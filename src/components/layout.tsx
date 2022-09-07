c76ce12103100ba910b8eb69482f598ffcb5d264
jeffloop <jeff@looparch.com>
2022-09-07 11:54:18 -0700

    Adding temp header and footer

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Navbar from './navbar'
+import Footer from './footer'
 
 type LayoutProps = {
   children: JSX.Element
@@ -7,7 +9,9 @@ type LayoutProps = {
 export default function Layout({ children }: LayoutProps) {
   return (
     <div>
+      <Navbar />
       <section className="max-w-5xl mx-auto">{children}</section>
+      <Footer />
     </div>
   )
 }
