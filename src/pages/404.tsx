4563acc04bc5ccf0270a18305d8c998a275ec6d0
jeffloop <jeff@looparch.com>
2023-06-16 10:34:04 -0700

    Adding 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,47 +1,20 @@
 import * as React from 'react'
-import { Link, HeadFC } from 'gatsby'
+import { HeadFC } from 'gatsby'
+import Layout from '../components/layout'
 import { SEO } from '../components/seo'
-
-const pageStyles = {
-  color: '#232129',
-  padding: '96px',
-  fontFamily: '-apple-system, Roboto, sans-serif, serif',
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: '#8A6534',
-  padding: 4,
-  backgroundColor: '#FFF4DB',
-  fontSize: '1.25rem',
-  borderRadius: 4,
-}
+import MeiliSearch from '../components/meilisearch'
 
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === 'development' ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <Layout>
+      <div className="max-w-6xl px-4 py-6 mx-auto sm:px-6 lg:py-8 lg:px-8">
+        <h1 className="mt-2 mb-6 text-3xl font-bold tracking-tight sm:text-4xl">
+          Whoops!
+        </h1>
+        <p>Page not found. Try searching for something else.</p>
+        <MeiliSearch />
+      </div>
+    </Layout>
   )
 }
 
